dfdf66771e38dada8e7fe3a250220526eb82ebdc
Vinicius <viniciusvieiradearaujo@hotmail.com>
2023-09-09 18:31:21 -0300

    Support for portuguese, chinese and russian

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ var worker;
 var start_time;
 var total_steps;
 var total_tries;
+var languageJson;
+
 
 window.onload = function() {
+    
     worker = new Worker("work.js?5");
     worker.onmessage = function(event) {
         if (event.data.msg == "complete") {
@@ -17,10 +20,12 @@ window.onload = function() {
         data: data
     });
 
+    
     buildItemSelection();
     buildEnchantmentSelection();
     buildCalculateButton();
     buildFilters();
+    setupLanguage();
 };
 
 function buildCalculateButton() {
@@ -33,9 +38,10 @@ function buildFilters() {
 }
 
 function buildItemSelection() {
+    //convert json into map
     const item_namespace2style = data.items;
     const item_namespaces = Object.keys(item_namespace2style);
-
+    
     item_namespaces.forEach(item_namespace => {
         const item_name = item_namespace2style[item_namespace];
         const item_listbox_metadata = { value: item_namespace };
@@ -46,6 +52,7 @@ function buildItemSelection() {
 
 function incompatibleGroupFromNamespace(enchantment_namespace) {
     const enchantments_metadata = data.enchants;
+    
     var incompatible_namespaces_queue = [enchantment_namespace];
     var incompatible_namespaces = [];
 
@@ -54,9 +61,9 @@ function incompatibleGroupFromNamespace(enchantment_namespace) {
         const incompatible_already_grouped = incompatible_namespaces.includes(incompatible_namespace);
 
         if (!incompatible_already_grouped) {
-            incompatible_namespaces.push(incompatible_namespace);
+            incompatible_namespaces.push(incompatible_namespace);            
             const enchantment_metadata = enchantments_metadata[incompatible_namespace];
-            const new_incompatible_namespaces = enchantment_metadata.incompatible;
+            var new_incompatible_namespaces = enchantment_metadata.incompatible; 
 
             new_incompatible_namespaces.forEach(new_incompatible_namespace => {
                 const new_incompatible_already_grouped = incompatible_namespaces.includes(new_incompatible_namespace);
@@ -87,6 +94,7 @@ function buildEnchantList(item_namespace_chosen) {
 
     const enchantment_namespaces = Object.keys(enchantments_metadata);
     enchantment_namespaces.forEach(enchantment_namespace => {
+
         const enchantment_metadata = enchantments_metadata[enchantment_namespace];
         const item_namespaces = enchantment_metadata.items;
 
@@ -144,11 +152,11 @@ function buildEnchantList(item_namespace_chosen) {
             const enchantment_metadata = enchantments_metadata[enchantment_namespace];
             const enchantment_max_level = enchantment_metadata.levelMax;
             const enchantment_name = enchantment_metadata["stylized"];
+            
 
             var enchantment_row = $("<tr>");
-            enchantment_row.addClass(group_toggle_color ? "group1" : "group2");
-
-            enchantment_row.append($("<td>").append(enchantment_name));
+            enchantment_row.addClass(group_toggle_color ? "group1" : "group2");            
+            enchantment_row.append($("<td>").append(languageJson.enchants[enchantment_namespace].stylized));
             for (let enchantment_level = 1; enchantment_level <= enchantment_level_maxmax; enchantment_level++) {
                 if (enchantment_max_level >= enchantment_level) {
                     const enchantment_button_data = {
@@ -234,11 +242,11 @@ function displayTime(time_milliseconds) {
         time_text = Math.round(time_microseconds) + " microseconds";
     } else if (time_milliseconds < 1000) {
         const time_round = Math.round(time_milliseconds);
-        time_text = time_round + " millisecond";
+        time_text = time_round + languageJson.millisecond;
         if (time_round != 1) time_text += "s";
     } else {
         const time_seconds = Math.round(time_milliseconds / 1000);
-        time_text = time_seconds + " second";
+        time_text = time_seconds + languageJson.second;
         if (time_seconds != 1) time_text += "s";
     }
 
@@ -246,7 +254,7 @@ function displayTime(time_milliseconds) {
 }
 
 function displayLevelsText(levels) {
-    var level_text = levels + " level";
+    var level_text = levels + languageJson.level;
     if (levels != 1) {
         level_text += "s";
     }
@@ -290,9 +298,9 @@ function displayInstructionText(instruction) {
     const right_item_text = displayItemText(right_item_obj);
     const prior_work_penalty = 2 ** prior_work - 1;
 
-    const instruction_text = "Combine <i>" + left_item_text + "</i> with <i>" + right_item_text + "</i>";
-    const cost_text = "Cost: " + displayLevelXpText(levels, xp);
-    const prior_work_text = "Prior Work Penalty: " + displayLevelsText(prior_work_penalty);
+    const instruction_text = languageJson.combine + " <i>" + left_item_text + "</i> " + languageJson.with + " <i>" + right_item_text + "</i>";
+    const cost_text = languageJson.cost + displayLevelXpText(levels, xp);
+    const prior_work_text = languageJson.prior_work_penalty + displayLevelsText(prior_work_penalty);
 
     const display_text = instruction_text + "<br><small>" + cost_text + ", " + prior_work_text + "</small>";
     return display_text;
@@ -300,7 +308,7 @@ function displayInstructionText(instruction) {
 
 function displayEnchantmentText(enchantment_obj) {
     const enchantment_namespace = enchantment_obj.namespace;
-    const enchantment_metadata = data.enchants[enchantment_namespace];
+    const enchantment_metadata = languageJson.enchants[enchantment_namespace];
     const enchantment_name = enchantment_metadata["stylized"];
     const enchantment_max_level = enchantment_metadata.levelMax;
 
@@ -331,7 +339,7 @@ function displayEnchantmentsText(enchantments_obj) {
 function displayItemText(item_obj) {
     const item_namespace = item_obj.item_namespace;
     const icon_text = '<img src="./images/' + item_namespace + '.gif" class="icon">';
-    const items_metadata = data.items;
+    const items_metadata = languageJson.items;
     const item_name = items_metadata[item_namespace];
 
     const enchantments_obj = item_obj.enchantments_obj;
@@ -342,7 +350,7 @@ function displayItemText(item_obj) {
 }
 
 function updateTime(time_milliseconds) {
-    const timing_text = "Completed in " + displayTime(time_milliseconds);
+    const timing_text = languageJson.completed_in + displayTime(time_milliseconds);
     $("#timings").text(timing_text);
     $("#timings").show();
 }
@@ -379,11 +387,11 @@ function afterFoundOptimalSolution(msg) {
     solution_section.show();
 
     if (instructions_count === 0) {
-        solution_header.html("No solution found!");
+        solution_header.html(languageJson.no_solution_found);
         steps_header.html("");
         updateCumulativeCost(0, 0);
     } else {
-        steps_header.html("Steps");
+        steps_header.html(languageJson.steps);
 
         const final_item_obj = msg.item_obj;
         const cumulative_levels = final_item_obj.cumulative_levels;
@@ -483,7 +491,7 @@ function updateLevelButtonForOnState(level_button) {
     if (!allow_incompatible) {
         const enchantment_namespace = enchantmentNamespaceFromStylized(enchantment_name);
         const enchantment_metadata = enchantments_metadata[enchantment_namespace];
-        const incompatible_namespaces = enchantment_metadata.incompatible;
+        var incompatible_namespaces = enchantment_metadata.incompatible;       
         filterEnchantmentButtons(incompatible_namespaces);
     }
 }
@@ -507,9 +515,9 @@ function levelButtonClicked(button_clicked) {
 
         if (is_too_many) {
             var alert_text = "";
-            alert_text += "Too many enchantments!";
-            alert_text += " More than " + ENCHANTMENT_LIMIT_INCLUSIVE + " enchantments are not recommended.";
-            alert_text += " Please deselect some enchantments or check the override near the bottom of the page.";
+            alert_text += languageJson.too_many_enchantments;
+            alert_text += languageJson.more_than + ENCHANTMENT_LIMIT_INCLUSIVE + languageJson.enchantments_are_not_recomended;
+            alert_text += languageJson.plese_select_enchantments;
             alert(alert_text);
         } else {
             updateLevelButtonForOnState(button_clicked);
@@ -564,9 +572,9 @@ function calculate() {
 function solutionHeaderTextFromMode(mode) {
     let solution_header_text;
     if (mode === "levels") {
-        solution_header_text = "Optimal solution found (by lowest cumulative levels)!";
+        solution_header_text = languageJson.optimal_solution_cumulative_levels;
     } else if (mode === "prior_work") {
-        solution_header_text = "Optimal solution found (by lowest prior work)!";
+        solution_header_text = languageJson.optimal_solution_prior_work;
     }
     return solution_header_text;
 }
@@ -603,6 +611,115 @@ function startCalculating(item_namespace, enchantment_foundation, mode) {
         mode: mode
     });
 
-    $("#progress .lbl").text("Calculating solution...");
+    $("#progress .lbl").text(languageJson.calculating_solution);
     $("#progress").show();
+}
+
+function languageChangeListener(){
+    const selectLanguage = document.getElementById('language');    
+    selectLanguage.addEventListener('change', function() {
+        const selectedValue = selectLanguage.value;
+        changePageLanguage(selectedValue);
+    });
+}
+
+async function setupLanguage(){    
+    defineBrowserLanguage();
+    languageChangeListener();
+}
+
+function defineBrowserLanguage(){
+    const browserLanguage = navigator.language || navigator.userLanguage;    
+    changePageLanguage(browserLanguage);
+}
+
+async function changePageLanguage(language){
+    languageJson = await loadJsonLanguage(language).then(languageData => { return languageData});
+    chageLanguageByJson(languageJson);
+}
+
+function loadJsonLanguage(language) {
+    return fetch('languages/'+language+'.json')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Can\' load file');
+        }
+        return response.json();
+      })
+      .then(data => {
+       
+        return data;
+      })
+      .catch(error => {
+        console.error('Language file error:', error);
+        return null; 
+      });
+}
+
+
+function chageLanguageByJson(languageJson){
+    const h1Element = document.getElementsByTagName('h1')[0];
+    h1Element.textContent = languageJson.h1_title;
+
+    /* paragraphs */
+    const paragraphs = document.getElementsByTagName('p');
+    paragraphs[1].innerHTML = languageJson.paragraph_1;
+
+    paragraphs[2].innerHTML = languageJson.paragraph_2;
+    
+    paragraphs[3].innerHTML = languageJson.paragraph_3;
+
+
+    /* selection */
+    const selectItem = document.getElementById("item");
+    selectItem.getElementsByTagName("option")[0].textContent = languageJson.choose_an_item_to_enchant;
+    selectItem.getElementsByTagName("option")[1].textContent = languageJson.items.helmet;
+    selectItem.getElementsByTagName("option")[2].textContent = languageJson.items.chestplate;
+    selectItem.getElementsByTagName("option")[3].textContent = languageJson.items.leggings;
+    selectItem.getElementsByTagName("option")[4].textContent = languageJson.items.boots;
+    selectItem.getElementsByTagName("option")[5].textContent = languageJson.items.turtle_shell;
+    selectItem.getElementsByTagName("option")[6].textContent = languageJson.items.elytra;
+    selectItem.getElementsByTagName("option")[7].textContent = languageJson.items.sword;
+    selectItem.getElementsByTagName("option")[8].textContent = languageJson.items.axe;
+    selectItem.getElementsByTagName("option")[9].textContent = languageJson.items.trident;
+    selectItem.getElementsByTagName("option")[10].textContent = languageJson.items.pickaxe;
+    selectItem.getElementsByTagName("option")[11].textContent = languageJson.items.shovel;
+    selectItem.getElementsByTagName("option")[12].textContent = languageJson.items.hoe;
+    selectItem.getElementsByTagName("option")[13].textContent = languageJson.items.bow;
+    selectItem.getElementsByTagName("option")[14].textContent = languageJson.items.shield;
+    selectItem.getElementsByTagName("option")[15].textContent = languageJson.items.crossbow;
+    selectItem.getElementsByTagName("option")[16].textContent = languageJson.items.brush;
+    selectItem.getElementsByTagName("option")[17].textContent = languageJson.items.fishing_rod;
+    selectItem.getElementsByTagName("option")[18].textContent = languageJson.items.shears;
+    selectItem.getElementsByTagName("option")[19].textContent = languageJson.items.flint_and_steel;
+    selectItem.getElementsByTagName("option")[20].textContent = languageJson.items.carrot_on_a_stick;
+    selectItem.getElementsByTagName("option")[21].textContent = languageJson.items.warped_fungus_on_a_stick;
+    selectItem.getElementsByTagName("option")[22].textContent = languageJson.items.pumpkin;
+    selectItem.getElementsByTagName("option")[23].textContent = languageJson.items.book;
+
+
+    const checkboxDiv = document.getElementById("overrides");
+    const labelsCheckBox = checkboxDiv.getElementsByTagName('label');
+    labelsCheckBox[0].innerHTML = languageJson.checkbox_label_0;
+    labelsCheckBox[1].innerHTML = languageJson.checkbox_label_1;
+    
+    document.getElementById("calculate").textContent = languageJson.calculate;
+
+    const modeSelection = document.getElementById("mode_selection");
+    
+    modeSelection.textContent = languageJson.optimize_for
+    
+    var labelRadio1 = document.createElement("label");
+    var labelRadio2 = document.createElement("label");
+
+    labelRadio1.innerHTML = languageJson.label_radio_1;
+    labelRadio2.innerHTML = languageJson.label_radio_2;
+
+    modeSelection.appendChild(labelRadio1);
+    modeSelection.appendChild(labelRadio2);
+
+    const solutionDiv = document.getElementById("solution")
+    solutionDiv.getElementsByTagName("b")[0].textContent = languageJson.total_cost;
+
+    document.getElementById("xp-range-note").textContent = languageJson.note;
 }
