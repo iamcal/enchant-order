a3686bf16733706ca685ad34f59c97d2005b71d3
Giuseppe Mattia Greco <giumatt99@gmail.com>
2024-08-20 11:11:36 +0200

    Added translations for Mace enchantments and added vanishing curse to Mace ench. list

--- data.js
+++ data.js
@@ -240,7 +240,7 @@ var data = {
             incompatible: [],
             items: ["helmet", "chestplate", "leggings", "boots", "pickaxe", "shovel", "axe", "sword", "hoe", "brush", "fishing_rod",
                 "bow", "shears", "flint_and_steel", "carrot_on_a_stick", "warped_fungus_on_a_stick", "shield", "elytra", "pumpkin",
-                "helmet", "trident", "turtle_shell", "crossbow"
+                "helmet", "trident", "turtle_shell", "crossbow", "mace"
             ]
         },
         density: {
